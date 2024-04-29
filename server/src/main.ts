5f5a68ed916fd417a12ef8177901d4f02e137b14
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2024-04-30 01:24:10 +0600

    Graphql integrated

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as cookieParser from 'cookie-parser'
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: '*',
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'X-Requested-with',
+      'apollo-require-preflight',
+    ]
+  });
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formatedErrors = errors.reduce((accumulator, error) => {
+          accumulator[error.property] = Object.values(error.constraints).join(
+            ', '
+          )
+          return accumulator;
+        }, {})
+        throw new BadRequestException(formatedErrors);
+      }
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
