fca8d59568f866849c4b34a056e4a96abe1279a3
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2024-05-07 01:05:19 +0600

    Graphql intigration in progress

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserResolver } from './user.resolver';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
-  providers: [UserService]
+  imports: [
+    AuthModule
+  ],
+  providers: [UserService, AuthService, UserResolver, JwtService, ConfigService, PrismaService]
 })
 export class UserModule {}
--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -1,4 +1,64 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthService } from 'src/auth/auth.service';
+import { UserService } from './user.service';
+import { LoginResponse, RegisterResponse } from 'src/auth/dto/response.dto';
+import { LoginDto, RegisterDto } from 'src/auth/dto/auth.dto';
+import { BadRequestException } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+    constructor(
+        private readonly authService: AuthService,
+        private readonly userService: UserService
+    ) { }
+
+    @Mutation(() => RegisterResponse)
+    async register(
+        @Args('registerInput') registerDto: RegisterDto,
+        @Context() context: { res: Response },
+    ): Promise<RegisterResponse> {
+        if (registerDto.password !== registerDto.confirmPassword) {
+            throw new BadRequestException({
+                confirmPassword: "Password and confirm password are not the same",
+            })
+        }
+        try {
+            const { user } = await this.authService.register(
+                registerDto,
+                context.res
+            );
+            return { user };
+        } catch (error) {
+            return {
+                error: {
+                    message: error.message
+                }
+            }
+        }
+    }
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Args('loginInput') loginDto: LoginDto,
+        @Context() Context: { res: Response }
+    ) {
+        return this.authService.login(loginDto, Context.res)
+    }
+
+    @Mutation(() => LoginResponse)
+    async logout(
+        @Context() Context: { res: Response }
+    ) {
+        return this.authService.logout(Context.res)
+    }
+
+    @Mutation(()=>String)
+    async refreshToken(@Context() Context: {req: Request, res: Response}){
+        try{
+            return this.authService.refreshToken(Context.req, Context.res)
+        }catch(error){
+            throw new BadRequestException(error.message);
+        }
+    }
+}
